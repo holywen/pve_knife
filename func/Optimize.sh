ddb561386a559ce70c452bddf24189eb5cd53080
Shaohua Wen <shaohua.wen@gmail.com>
2019-10-17 12:42:45 +0800

    update to buster

--- func/Optimize.sh
+++ func/Optimize.sh
@@ -11,21 +11,21 @@ InstallBasicComponent() {
 ReplaceEnterpriseSource() {
     if [ -f "/etc/apt/sources.list.d/pve-enterprise.list"  ]; then
     mv /etc/apt/sources.list.d/pve-enterprise.list /etc/apt/sources.list.d/pve-enterprise.list.bak
-    echo -e 'deb http://download.proxmox.com/debian/pve stretch pve-no-subscription\n' > /etc/apt/sources.list.d/pve-community.list
+    echo -e 'deb http://download.proxmox.com/debian/pve buster pve-no-subscription\n' > /etc/apt/sources.list.d/pve-community.list
     fi
     echo "Source replacement already complete"
 }
 
 ReplaceDebianUpdateRepo() {
     cat > /etc/apt/sources.list <<EOF
-deb https://mirrors.aliyun.com/debian/ stretch main non-free contrib
-deb-src https://mirrors.aliyun.com/debian/ stretch main non-free contrib
-deb https://mirrors.aliyun.com/debian-security stretch/updates main
-deb-src https://mirrors.aliyun.com/debian-security stretch/updates main
-deb https://mirrors.aliyun.com/debian/ stretch-updates main non-free contrib
-deb-src https://mirrors.aliyun.com/debian/ stretch-updates main non-free contrib
-deb https://mirrors.aliyun.com/debian/ stretch-backports main non-free contrib
-deb-src https://mirrors.aliyun.com/debian/ stretch-backports main non-free contrib
+deb https://mirrors.aliyun.com/debian/ buster main non-free contrib
+deb-src https://mirrors.aliyun.com/debian/ buster main non-free contrib
+deb https://mirrors.aliyun.com/debian-security buster/updates main
+deb-src https://mirrors.aliyun.com/debian-security buster/updates main
+deb https://mirrors.aliyun.com/debian/ buster-updates main non-free contrib
+deb-src https://mirrors.aliyun.com/debian/ buster-updates main non-free contrib
+deb https://mirrors.aliyun.com/debian/ buster-backports main non-free contrib
+deb-src https://mirrors.aliyun.com/debian/ buster-backports main non-free contrib
 EOF
 }
 
